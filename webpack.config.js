8d6681d87030c328d88a657de6568e31d8037bd3
Troy Grossi <ttroygrossit@gmail.com>
2021-09-14 15:03:40 -0700

    added webpack to minify the javascript

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require("path");
+const webpack = require("webpack");
+
+
+module.exports = {
+    entry: "./assets/js/script.js",
+     output: {
+         path: path.resolve(__dirname, 'dist'),
+         filename: 'main.bundle.js'
+     },
+     mode: 'development',
+    };
